34c6fc606c68ea9acbbe49734bf30ecb2c280456
Wojciech Bębenek <wojciech.bebenek@ac-project.net>
2019-11-29 11:55:37 +0100

    Updates

---
+++ precache-manifest.eb4f8dae32fb438a3e331076bed95dc1.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1152aa136ce7a9f73ec7b8c2834ef52",
+    "url": "/boostt/index.html"
+  },
+  {
+    "revision": "25c6c238b30c663bc2b9",
+    "url": "/boostt/static/css/main.1a8d6906.chunk.css"
+  },
+  {
+    "revision": "e95d17f3ca6abf52e67b",
+    "url": "/boostt/static/js/2.b3b5d8c0.chunk.js"
+  },
+  {
+    "revision": "25c6c238b30c663bc2b9",
+    "url": "/boostt/static/js/main.c706b089.chunk.js"
+  },
+  {
+    "revision": "6846d0ccf47f082856cd",
+    "url": "/boostt/static/js/runtime-main.686af2bc.js"
+  },
+  {
+    "revision": "cc169cc48c13f8b9158ef61192105b8b",
+    "url": "/boostt/static/media/american.cc169cc4.png"
+  },
+  {
+    "revision": "666be55123fa7870d06fab80060690e2",
+    "url": "/boostt/static/media/americanPhotography.666be551.png"
+  },
+  {
+    "revision": "d3aaa54cd92885f4333d5f7a3c13c378",
+    "url": "/boostt/static/media/arrow.d3aaa54c.svg"
+  },
+  {
+    "revision": "4d7f2bc69a3725f8ad0d72c3ee0d1d3f",
+    "url": "/boostt/static/media/background.4d7f2bc6.png"
+  },
+  {
+    "revision": "882c8962f2dfc0355978152fe3ae0cff",
+    "url": "/boostt/static/media/calendar.882c8962.svg"
+  },
+  {
+    "revision": "97c8b1f78bd044ae659679e02814c691",
+    "url": "/boostt/static/media/chevron.97c8b1f7.svg"
+  },
+  {
+    "revision": "bff61d49c1e35c3cfc5fff587b7fdd6d",
+    "url": "/boostt/static/media/clock.bff61d49.svg"
+  },
+  {
+    "revision": "e78c0803b91b894bf527cdde1038cf77",
+    "url": "/boostt/static/media/cube.e78c0803.svg"
+  },
+  {
+    "revision": "77dcb95e5304c6669516c6ea2deb0e3b",
+    "url": "/boostt/static/media/cubism.77dcb95e.png"
+  },
+  {
+    "revision": "b70bfb0fc601d6abfdceba0a0f15e907",
+    "url": "/boostt/static/media/decorative.b70bfb0f.png"
+  },
+  {
+    "revision": "8a95778ebbd51ff7eb15dad03c7680a8",
+    "url": "/boostt/static/media/destijl.8a95778e.png"
+  },
+  {
+    "revision": "942e53cf00872e058d903ef1ddb82a08",
+    "url": "/boostt/static/media/greek.942e53cf.png"
+  },
+  {
+    "revision": "3e8569b8f8c903addc707908acba41c1",
+    "url": "/boostt/static/media/masters.3e8569b8.png"
+  },
+  {
+    "revision": "f9e465b72509eb5b70b5adc5545d8ac5",
+    "url": "/boostt/static/media/memberCard.f9e465b7.svg"
+  },
+  {
+    "revision": "f6d1412d2608d895e46369bc04a45a62",
+    "url": "/boostt/static/media/menu.f6d1412d.svg"
+  },
+  {
+    "revision": "f86a84ee11693267063c2ae34799fd21",
+    "url": "/boostt/static/media/minus.f86a84ee.svg"
+  },
+  {
+    "revision": "e7e6d91dce65953b307cd6116a517ce2",
+    "url": "/boostt/static/media/pin.e7e6d91d.svg"
+  },
+  {
+    "revision": "0931376381f67a6c8970cdeb7d4c2643",
+    "url": "/boostt/static/media/plus.09313763.svg"
+  },
+  {
+    "revision": "80f878d5221404beb510db593f3c2513",
+    "url": "/boostt/static/media/search.80f878d5.svg"
+  },
+  {
+    "revision": "69b0b7a13e02e06380c13cd5115d8390",
+    "url": "/boostt/static/media/shapes.69b0b7a1.svg"
+  },
+  {
+    "revision": "3ab021b20200fccbb46cf64d630f325f",
+    "url": "/boostt/static/media/shop.3ab021b2.svg"
+  },
+  {
+    "revision": "ac7fc3e47b07658092d38dbe15be47b3",
+    "url": "/boostt/static/media/ticket.ac7fc3e4.svg"
+  }
+]);
